20931f00be6c4d1a0dc509daea45e83978140054
Vince Reef <devreef@protonmail.com>
2021-11-26 15:04:03 +0100

    Modification Site

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,27 +8,24 @@ import mail from '../../assets/SocialMedias/mail.svg';
 export default function Header() {
   return (
     <header className={style.headerWrapper}>
-      
       <div className={style.leftMenuWrapper}>
         <h4 className={style.menuLabels}>GM VITIAGRI</h4>
       </div>
       <div className={style.rightMenuWrapper}>
         <div className={style.iconfacebookWrapper}>
-         
-            <a href="https://www.facebook.com/gm.vitiagri" target="_blank">
-              <Image
-                src={facebook}
-                alt="facebook"
-                layout="responsive"
-                width={25}
-                height={25}
-              />
-            </a>
-   
+          <a href="https://www.facebook.com/gm.vitiagri" target="_blank" rel="noreferrer">
+            <Image
+              src={facebook}
+              alt="facebook"
+              layout="responsive"
+              width={25}
+              height={25}
+            />
+          </a>
         </div>
 
         <div className={style.iconWrapper}>
-          <Link href="mailto:faurie.guillaume33@gmail.com" >
+          <Link href="mailto:faurie.guillaume33@gmail.com">
             <a>
               <Image
                 src={mail}
